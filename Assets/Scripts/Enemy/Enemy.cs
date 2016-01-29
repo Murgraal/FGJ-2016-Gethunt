d3f3c4450a8ac262e50bd6e30bcf53758e919515
santeri Honkanen <santeri.honkanen@edu.karelia.fi>
2016-01-29 22:02:11 +0200

    Repo Initalization

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public float _morale;
+    public float dmg;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_morale <= 0.0f)
+        {
+            KillPlayer();
+        }
+    }
+
+    public void ReduceMorare(float _reduce)
+    {
+        _morale -= _reduce;
+    }
+
+    public void Attack()
+    {
+        //implement player morale reduce here
+    }
+
+    public void Talk(string _dialogue)
+    {
+        //implement dialogue here
+    }
+
+    public void KillPlayer()
+    {
+        //trigger death & reset
+    }
+}
---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TargetPerson { Bartender, CockBlocker, Portsari, Babe };
+
+public class PlayerCombat : MonoBehaviour {
+
+    public static PlayerCombat Instance = null;
+
+    public List<string> _Playermsgs;
+    TargetPerson _targetPersons;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+
+        }
+    }
+
+    public void Attack()
+    {
+        //Implement PlayerAttack();
+    }
+}
