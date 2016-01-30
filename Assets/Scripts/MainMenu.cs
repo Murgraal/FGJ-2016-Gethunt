70608e9312ef00f42ea44a7319aaabbbac41fb43
Stu <vivant.studiosi@gmail.com>
2016-01-30 11:59:57 +0200

    Main Menu and Sound Manager
    Untested

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SoundPanel.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundPanel : MonoBehaviour {
+
+    public static SoundPanel sp = null;
+
+    public AudioClip[] audioSelection;
+    public AudioSource audioPlayer;
+
+    public void Awake()
+    {
+        if(sp == null)
+        {
+            sp = this;
+        }
+        else if (sp != this)
+        {
+            DestroyObject(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    public void PlaySingle(int index)
+    {
+        audioPlayer.clip = audioSelection[index];
+        audioPlayer.Play();     
+    }
+
+    public void PlaySingle(string name)
+    {
+        AudioClip selected = null;
+
+        foreach(AudioClip au in audioSelection)
+        {
+            if(au.name == name)
+            {
+                selected = au;
+                break;
+            }
+        }
+
+        if (selected != null)
+        {
+            audioPlayer.clip = selected;
+            audioPlayer.Play();
+        }
+
+    }
+
+
+}
