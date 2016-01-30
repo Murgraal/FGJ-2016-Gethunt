a6fd423dbd4ac233ba3702714895be852238fd8e
Stu <vivant.studiosi@gmail.com>
2016-01-30 15:27:50 +0200

    SM enhanced

--- Assets/Scripts/SlotMachine.cs
+++ Assets/Scripts/SlotMachine.cs
@@ -5,6 +5,7 @@ public class SlotMachine : MonoBehaviour
 {
 
     private float winningProbability = 0.3f;
+    public SpriteRenderer[] sr;
 
     public void use(PlayerData player)
     {
@@ -27,20 +28,44 @@ public class SlotMachine : MonoBehaviour
 
             if (result == 0)
             {
+                setRandomGraphicsWinning1();
                 player.money += 2;
             }
             else if (result == 1)
             {
+                setRandomGraphicsWinning2();
                 player.money += 3;
             }
             else
             {
+                setRandomGraphicsWinning3();
                 player.money += 4;
             }
 
         }
+        else
+        {
+            setCrapRandomGraphics();
+        }
 
     }
 
+    public void setCrapRandomGraphics()
+    {
+    }
 
+    public void setRandomGraphicsWinning1()
+    {
+    }
+
+    public void setRandomGraphicsWinning2()
+    {
+    }
+
+    public void setRandomGraphicsWinning3()
+    {
+
+
+
+    }
 }
