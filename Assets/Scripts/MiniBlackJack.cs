61487a9b22761c2830690f05bff1472b93fa5b0a
Kim Ordén <kim.orden@edu.pkamk.fi>
2016-01-31 00:14:09 +0200

    spärdärijablackjack

--- Assets/Scripts/MiniBlackJack.cs
+++ Assets/Scripts/MiniBlackJack.cs
@@ -5,25 +5,32 @@ using System;
 public class MiniBlackJack : MonoBehaviour {
 
     public GameObject[] cards;
-    public GameObject[] dealerHand;
-    public GameObject[] playerHand;
-    public int nextcard;   
+    public SpriteRenderer[] dealerNumber,dealerSymbol;
+    public SpriteRenderer[] playerNumber, playerSymbol;
+    public Sprite[] numbers;
+    public Sprite[] symbols;
+    public int nextcard;
 
     public void startHand()
     {
-        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
-        dealerHand[0] = cards[nextcard];
-        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
-        dealerHand[1] = cards[nextcard];
-        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
-        playerHand[0] = cards[nextcard];
-        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
-        playerHand[1] = cards[nextcard];
 
-        int playerValue = calculateValue(playerHand);
-        int dealerValue = calculateValue(dealerHand);
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        dealerNumber[0].sprite = numbers[nextcard];
+        dealerSymbol[0].sprite = symbols[UnityEngine.Random.Range(0, symbols.Length - 1)];
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        dealerNumber[1].sprite = numbers[nextcard];
+        dealerSymbol[1].sprite = symbols[UnityEngine.Random.Range(0, symbols.Length - 1)];
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        playerNumber[0].sprite = numbers[nextcard];
+        playerSymbol[0].sprite = symbols[UnityEngine.Random.Range(0, symbols.Length - 1)];
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        playerNumber[1].sprite = numbers[nextcard];
+        playerSymbol[1].sprite = symbols[UnityEngine.Random.Range(0, symbols.Length - 1)];
 
-        if(playerValue > dealerValue)
+        int playerValue = calculateValue(playerNumber);
+        int dealerValue = calculateValue(dealerNumber);
+
+        if (playerValue > dealerValue)
         {
             print("YOU WON");
         }
@@ -42,14 +49,28 @@ public class MiniBlackJack : MonoBehaviour {
         }
     }
 
-    public int calculateValue(GameObject[] hand)
+    public int calculateValue(SpriteRenderer[] hand)
     {
 
         int i = 0;
 
-        foreach(GameObject card in hand)
+        foreach (SpriteRenderer number in hand)
         {
-            i += Int32.Parse(card.name);
+            int x = (Int32.Parse(number.sprite.name.Split(new char[] { '_' })[1]) + 1);
+
+            if (x == 13)
+            {
+                i += 1;
+            }
+            else if(x >= 10)
+            {
+                i += 10;
+            } 
+            else
+            {
+                i += x;
+            }            
+
         }
 
         return i;
---
+++ Assets/Scripts/Spärdäriscript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spardariscript : MonoBehaviour {
+
+    float speed;
+    public Vector2 mouspos;
+    public GameObject spärdäri;
+    public Vector2 direction;
+    public GameObject spärdärilocation,ledique;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            direction = mouspos - (Vector2)spärdärilocation.transform.position;
+            spärdäri.GetComponent<Rigidbody2D>().AddForce(direction);
+            mouspos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        
+        if (Vector2.Distance(spärdärilocation.transform.position, ledique.transform.position) < 3)
+        {
+            print("success");
+        }
+	}
+}
