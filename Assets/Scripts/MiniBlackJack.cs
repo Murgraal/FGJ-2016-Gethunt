f4542fe1dccf9f731de5ea6b409d784c5f2da6fc
Stu <vivant.studiosi@gmail.com>
2016-01-30 19:26:22 +0200

    MiniBJ
    MiniBJ created and teste

--- Assets/Scripts/MiniBlackJack.cs
+++ Assets/Scripts/MiniBlackJack.cs
@@ -1,15 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class MiniBlackJack : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject[] cards;
+    public GameObject[] dealerHand;
+    public GameObject[] playerHand;
+    public int nextcard;   
+
+    public void startHand()
+    {
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        dealerHand[0] = cards[nextcard];
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        dealerHand[1] = cards[nextcard];
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        playerHand[0] = cards[nextcard];
+        nextcard = UnityEngine.Random.Range(0, cards.Length - 1);
+        playerHand[1] = cards[nextcard];
+
+        int playerValue = calculateValue(playerHand);
+        int dealerValue = calculateValue(dealerHand);
+
+        if(playerValue > dealerValue)
+        {
+            print("YOU WON");
+        }
+        else
+        {
+            print("YOU LOSE");
+        }
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            startHand();
+        }
+    }
+
+    public int calculateValue(GameObject[] hand)
+    {
+
+        int i = 0;
+
+        foreach(GameObject card in hand)
+        {
+            i += Int32.Parse(card.name);
+        }
+
+        return i;
+
+    }
+
+    private int getCard()
+    {
+        return UnityEngine.Random.Range(0, cards.Length - 1);
+    }
+
 }
